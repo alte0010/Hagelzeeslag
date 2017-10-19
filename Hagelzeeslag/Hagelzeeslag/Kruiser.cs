7013b130ab8bf0f6815e930df9e85d4b2f349d14
Andries <nieu0082@hz.nl>
2017-10-19 20:44:24 +0200

    Unittest aan de praat.

---
+++ Hagelzeeslag/Hagelzeeslag/Kruiser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hagelzeeslag
+{
+    public class Kruiser
+    {
+        private int lives;
+
+        public Kruiser()
+        {
+            lives = 4;
+        }
+
+        public bool IsSunk()
+        {
+            return !(lives > 0);
+        }
+    }
+}
---
+++ Hagelzeeslag/UnitTestProject/KruiserTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hagelzeeslag;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class KruiserTest
+    {
+        [TestMethod]
+        public void TestInitialStateIsNotSunk()
+        {
+            // Arrange
+            var ship = new Kruiser();
+
+            // Act
+            bool isSunk = ship.IsSunk();
+
+            // Assert
+            Assert.IsFalse(isSunk);
+        }
+    }
+}
